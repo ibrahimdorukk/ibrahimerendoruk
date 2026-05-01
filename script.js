3b31f55e016e0366d7fa81b413d288fc053f0e43
ibrahimdorukk <ibrahim.drk.123@gmail.com>
2026-05-01 20:36:03 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+const menuBtn = document.getElementById("menuBtn");
+const mobileMenu = document.getElementById("mobileMenu");
+
+menuBtn.addEventListener("click", () => {
+  mobileMenu.classList.toggle("active");
+
+  if (mobileMenu.classList.contains("active")) {
+    menuBtn.textContent = "✕";
+  } else {
+    menuBtn.textContent = "☰";
+  }
+});
+
+document.querySelectorAll("#mobileMenu a").forEach(link => {
+  link.addEventListener("click", () => {
+    mobileMenu.classList.remove("active");
+    menuBtn.textContent = "☰";
+  });
+});
+
 console.log("Site çalışıyor");
