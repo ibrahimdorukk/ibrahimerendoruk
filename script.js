adea22585105696aa44087a0fdf86491d4b44932
ibrahimdorukk <ibrahim.drk.123@gmail.com>
2026-05-23 19:00:38 +0300

    Update script.js

--- script.js
+++ script.js
@@ -382,4 +382,4 @@ langButtons.forEach(button => {
 const savedLanguage = localStorage.getItem("siteLanguage") || "tr";
 setLanguage(savedLanguage);
 
-console.log("Çok dilli site çalışıyor");
+console.log("Stabil çok dilli site çalışıyor");
